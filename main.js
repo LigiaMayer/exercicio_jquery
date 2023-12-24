b2d6b94bd588b009ebf522035f961339ee49bc11
Ligia mayer rodrigues <ligia-angela@hotmail.com>
2023-12-23 22:54:10 -0300

    Commiti

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const novoItem = $('#newitem').val();
+        const itemLista = $('<li></li>').text(novoItem);
+        $('ul').append(itemLista);        
+        $('#novo-item').val('');
+    });
+
+    $('ul').on('click', 'li', function() {
+        $(this).toggleClass('riscado');
+    });
+});
